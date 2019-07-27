708c0b9a3af93feefb243f837b1f5f68a193513f
شاةثي <@.>
2019-07-27 19:08:44 +0200

    Updates

---
+++ precache-manifest.e6ab1e938ceacf629b294a3991d6f701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57add9098f39130bbc9737f37df0c010",
+    "url": "/course_list/index.html"
+  },
+  {
+    "revision": "3d6b872131d8ade9c02d",
+    "url": "/course_list/static/css/main.6d937645.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/course_list/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3d6b872131d8ade9c02d",
+    "url": "/course_list/static/js/main.366408f8.chunk.js"
+  },
+  {
+    "revision": "534612b01d3b1e5e2349",
+    "url": "/course_list/static/js/runtime~main.0de1ba45.js"
+  }
+]);
